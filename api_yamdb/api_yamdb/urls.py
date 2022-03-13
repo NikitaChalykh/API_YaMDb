8202566c79e25dc58333214e7b0a3e3b0ef40136
Никита Чалых <Wizardus@list.ru>
2022-03-13 12:03:52 +0300

    dssds

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,10 +1,10 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf.urls import url
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
