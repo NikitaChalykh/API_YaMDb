4db7644fd1142d141c1e74d5e7e33b5add01039a
Никита Чалых <Wizardus@list.ru>
2022-03-13 22:24:45 +0300

    Продолжаем настраивать ReDoc

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,11 +1,6 @@
-# from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-
-# from drf_yasg import openapi
-# from drf_yasg.views import get_schema_view
-# from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,24 +11,3 @@ urlpatterns = [
         name='redoc'
     ),
 ]
-
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="YaMDb",
-#         default_version='v1',
-#         description="Документация для приложения YaMDb",
-#         contact=openapi.Contact(email="admin@admin.ru"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
-
-# urlpatterns += [
-#     url(r'^swagger(?P<format>\.json|\.yaml)$',
-#         schema_view.without_ui(cache_timeout=0), name='schema-json'),
-#     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-#         name='schema-swagger-ui'),
-#     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-#         name='schema-redoc'),
-# ]
