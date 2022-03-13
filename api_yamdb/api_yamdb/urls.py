a0dcc8e892a76e78d57ff4cb2d048a9d34eba3ee
Никита Чалых <Wizardus@list.ru>
2022-03-13 19:41:55 +0300

    112

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -23,6 +23,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns += [
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+    url(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'),
 ]
