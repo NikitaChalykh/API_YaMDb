458d54608b5002d85fe1ec767c34215cc59b9597
Никита Чалых <Wizardus@list.ru>
2022-03-13 18:38:39 +0300

    крайний коммит

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
